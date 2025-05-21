7c6f8ae0338c8c53f46c21c68b21ba1d680c84ec
semartinl <semartinl@alumnos.unex.es>
2025-05-21 08:48:13 +0200

    Primer commit

---
+++ pages/timeline/index.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Timeline() {
+  return (
+    <div>
+      <h1>This is the timeline</h1> 
+      <Link href="/"> <a>Go to home</a></Link>
+    </div>
+  )
+}
